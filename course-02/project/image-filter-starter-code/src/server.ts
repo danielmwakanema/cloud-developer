d9afb67305f2e6f90f9e4631029614d9695c0163
Daniel Mwakanema <danielmwakanema95@gmail.com>
2021-09-30 13:22:56 +0200

    feat(app): add route to filter images

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,15 +1,18 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
+import { check, query, validationResult } from "express-validator";
+import { filterImageFromURL, deleteLocalFiles } from "./util/util";
+import { HTTP_STATUS } from "./common/types";
+import { HTTP_MESSAGES } from "./common/messages";
+import { existsSync } from "fs";
 
 (async () => {
-
   // Init the Express application
   const app = express();
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
@@ -17,7 +20,6 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
   // IT SHOULD
-  //    1
   //    1. validate the image_url query
   //    2. call filterImageFromURL(image_url) to filter the image
   //    3. send the resulting file in the response
@@ -30,17 +32,49 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   /**************************************************************************** */
 
   //! END @TODO1
-  
+
+  // fetch and filter image from url
+  app.get(
+    "/filteredImage",
+    [
+      check('image_url').notEmpty().withMessage('image_url is required.'),
+      check('image_url').isURL().withMessage('image_url must be a valid URL string.')
+    ],
+    async (req: Request, res: Response) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(HTTP_STATUS.UNPROCESSABLE).send({
+          statusCode: HTTP_STATUS.UNPROCESSABLE,
+          message: HTTP_MESSAGES.UNPROCESSABLE,
+          errors: errors.mapped(),
+        })
+      }
+
+      const { image_url } = req.query;
+
+      try {
+        const image = await filterImageFromURL(image_url);
+        return res.sendFile(image, () => deleteLocalFiles([image]));
+      } catch (e) {
+        return res
+          .status(HTTP_STATUS.SERVER_ERROR)
+          .send({
+            statusCode: HTTP_STATUS.SERVER_ERROR,
+            message: HTTP_MESSAGES.SERVER_ERROR,
+          });
+      }
+    }
+  );
+
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
-    res.send("try GET /filteredimage?image_url={{}}")
-  } );
-  
+  app.get("/", async (req, res) => {
+    res.send("try GET /filteredimage?image_url={{}}");
+  });
 
   // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
 })();
